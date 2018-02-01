1137236139a8159d949819f1f22acd331b15e6d9
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2018-02-01 14:58:46 -0500

    removing click (CLI plugin) and its references

--- tests/test_corticalqa.py
+++ tests/test_corticalqa.py
@@ -5,34 +5,4 @@
 
 import pytest
 
-from click.testing import CliRunner
-
-from visualqc import qa
-from visualqc import cli
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'visualqc.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+from visualqc import vqc
