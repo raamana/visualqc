0bf7e041adbfc5c0b063ea08e1ae425bf255eeef
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2018-02-01 14:58:13 -0500

    implementing basic CLI options and the skeleton

--- visualqc/__init__.py
+++ visualqc/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for visualqc."""
 
+__all__ = ['vqc', 'generate_visualizations', 'rate_visualizations']
+
 __author__ = """Pradeep Reddy Raamana"""
 __email__ = 'raamana@gmail.com'
 __version__ = '0.1.0'
@@ -9,3 +11,7 @@ __version__ = '0.1.0'
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+from visualqc import vqc
+from visualqc.vqc import generate_visualizations, rate_visualizations
