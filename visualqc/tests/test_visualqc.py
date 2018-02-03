7b43a1ae5a2b2a8faddeb1dc795d44437bb7f985
Pradeep Reddy Raamana <raamana@gmail.com>
2018-02-03 10:07:59 -0500

    super basic test for local runs

--- visualqc/tests/test_visualqc.py
+++ visualqc/tests/test_visualqc.py
@@ -11,10 +11,16 @@ aseg_path = pjoin(aseg_mri_dir, 'aparc+aseg.mgz')
 mri_path = pjoin(aseg_mri_dir, 'orig.mgz')
 out_aseg_mri_path = pjoin(base_dir,'aseg_overlay_on_mri_test')
 
-make_type = 'cortical_volumetric'
+vis_type = 'cortical_volumetric'
 fs_dir = base_dir
 id_list = ('id_001', )
 out_dir = pjoin(fs_dir,'qc_vis')
 makedirs(out_dir, exist_ok=True)
 
-generate_visualizations(make_type=make_type, fs_dir=fs_dir, id_list=id_list, out_dir=out_dir)
+def test_gen():
+    """super basic run."""
+
+    generate_visualizations(vis_type=vis_type, fs_dir=fs_dir, id_list=id_list, out_dir=out_dir,
+                            alpha_set=(0.8, 0.7))
+
+test_gen()
