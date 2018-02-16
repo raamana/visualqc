42974e86fcfd31b5530c5226d976634b191a8a15
Pradeep Reddy Raamana <raamana@gmail.com>
2018-02-16 09:48:39 -0500

    better setting up the tests [skip ci]

--- visualqc/tests/test_visualqc.py
+++ visualqc/tests/test_visualqc.py
@@ -1,29 +1,24 @@
 
-from os import mkdir, makedirs
+from os import makedirs
 from os.path import join as pjoin, exists as pexists, abspath, realpath, basename, dirname
-from visualqc.vqc import run_workflow, cli_run
+from visualqc.vqc import cli_run
 import shlex
 import sys
 
 test_dir = dirname(realpath(__file__))
 base_dir = realpath(pjoin(test_dir, '..', '..', 'example_datasets'))
 
-aseg_mri_dir = pjoin(base_dir,'id_001')
-aseg_path = pjoin(aseg_mri_dir, 'aparc+aseg.mgz')
-mri_path = pjoin(aseg_mri_dir, 'orig.mgz')
-out_aseg_mri_path = pjoin(base_dir,'aseg_overlay_on_mri_test')
+id_list = pjoin(base_dir, 'id_list')
+fs_dir = base_dir
 
-vis_type = 'cortical_contour'
-fs_dir = '/data1/strother_lab/praamana/ABIDE/processed/freesurfer_v5.1'
-id_list = '/data1/strother_lab/praamana/ABIDE/processed/target_lists/list.visualqc_n10.csv'
-out_dir = pjoin(fs_dir,'qc_vis')
+out_dir = pjoin(fs_dir,'vqc_test')
 makedirs(out_dir, exist_ok=True)
 
 
 def test_gen():
     "function to hit the CLI lines."
 
-    sys.argv = shlex.split('visualqc -f {} -i {} -o {} -v {}'.format(fs_dir, id_list, out_dir, vis_type))
+    sys.argv = shlex.split('visualqc -f {} -i {} -o {}'.format(fs_dir, id_list, out_dir))
     cli_run()
 
 
@@ -44,5 +39,5 @@ def test_gen_single_view():
 
 
 # test_gen()
-test_gen_label_focus()
+# test_gen_label_focus()
 # test_gen_single_view()
