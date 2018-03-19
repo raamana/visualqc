f9a7ca7cfd7523c604d422b6629058bbc4c74ad4
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2018-03-19 18:30:47 -0400

    new method for masking image - fmri foreground etc

--- visualqc/image_utils.py
+++ visualqc/image_utils.py
@@ -30,3 +30,25 @@ def gradient_magnitude(mri):
     grad_magnitude = np.sqrt(np.sum(np.power(grad, 2.), axis=0))
 
     return grad_magnitude
+
+
+def mask_image(input_img, init_percentile=2):
+    """
+    Estimates the foreground mask for a given image.
+
+    Similar to 3dAutoMask from AFNI.
+    """
+
+    prev_clip_level = np.percentile(input_img, init_percentile)
+    while True:
+        mask_img = input_img >= prev_clip_level
+        cur_clip_level = 0.5*np.median(input_img[mask_img])
+        if np.isclose(cur_clip_level, prev_clip_level, rtol=0.05):
+            break
+        else:
+            prev_clip_level = cur_clip_level
+
+    se36 = ndimage.generate_binary_structure(3, 6)
+    mask_img = ndimage.binary_closing(mask_img, se36, iterations=3)
+
+    return mask_img
