a3c79166af63b066d92b2c6f3936d5f2b19995fd
Pradeep Reddy Raamana <raamana@gmail.com>
2021-08-20 18:09:12 -0400

    typo

--- visualqc/diffusion.py
+++ visualqc/diffusion.py
@@ -1419,7 +1419,7 @@ def cli_run():
     """Main entry point."""
 
     from visualqc import __version__
-    print('\nvisualqc version {} for anatomical MRI'.format(__version__))
+    print('\nvisualqc version {} for diffusion MRI'.format(__version__))
     from datetime import datetime
     init_time = datetime.now()
     print('\tTime stamp : {}\n'.format(init_time.strftime('%Y-%m-%d %H:%M:%S')))
