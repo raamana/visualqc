9636fe1dbef9f0a06c97000c7370b382007f143d
Pradeep Reddy Raamana <raamana@gmail.com>
2022-02-03 17:42:25 -0500

    non-isotropic resolution test

--- visualqc/tests/test_defacing.py
+++ visualqc/tests/test_defacing.py
@@ -9,8 +9,8 @@ test_dir = dirname(realpath(__file__))
 base_dir = realpath(pjoin(test_dir, '..', '..', 'example_datasets'))
 
 id_list = pjoin(base_dir, 'id_list_defacing')
-name_defaced = 'defaced.nii.gz'
-name_mri = 'orig.nii.gz'
+name_defaced = 'defaced_noniso2.nii.gz' # 'defaced.nii.gz'
+name_mri = 'orig_noniso2.nii.gz' # 'orig.nii.gz'
 name_rendered = 'rendered.png'
 
 out_dir = pjoin(base_dir, 'out_defacing')
